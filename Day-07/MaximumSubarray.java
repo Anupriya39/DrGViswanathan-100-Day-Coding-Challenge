ff231edba2ba861442d4fc3a903f9bc63307c5b9
Anupriya <anupriya6115@gmail.com>
2026-09-07 22:10:11 +0530

    Day-07: Add Maximum Subarray

---
+++ Day-07/MaximumSubarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            
+            // Either start a new subarray or extend the current one
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            
+            // Update maximum sum
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
